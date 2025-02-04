a7d86a5f2a1e7ff79c044e9794d598f3360555c6
Vinicius Charles <viniciuscharles3101@gmail.com>
2025-02-04 14:59:02 -0300

    Projeto criado

---
+++ src/main/java/com/viniciuscharles/projetocursomc/ProjetocursomcApplication.java
@@ -0,0 +1,13 @@
+package com.viniciuscharles.projetocursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetocursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetocursomcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viniciuscharles/projetocursomc/ProjetocursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciuscharles.projetocursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetocursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
